00dfd12feff1a2efd2db2bcb1ea75684bfc3a13d
rhendz <patelankush.2012@gmail.com>
2019-02-15 09:56:13 -0500

    Adds UI for displaying messages #3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const String _name = "Rockstar Rick";
+
 void main() {
   runApp(new CampuschatApp());
 }
@@ -20,6 +22,7 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
 
   Widget _buildTextComposer() {
@@ -51,14 +54,66 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Campus Chat")),
+      appBar: new AppBar(title: new Text("Campuschat")),
+      body: new Column(                                        //modified
+        children: <Widget>[                                         //new
+          new Flexible(                                             //new
+            child: new ListView.builder(                            //new
+              padding: new EdgeInsets.all(8.0),                     //new
+              reverse: true,                                        //new
+              itemBuilder: (_, int index) => _messages[index],      //new
+              itemCount: _messages.length,                          //new
+            ),                                                      //new
+          ),                                                        //new
+          new Divider(height: 1.0),                                 //new
+          new Container(                                            //new
+            decoration: new BoxDecoration(
+                color: Theme.of(context).cardColor),                  //new
+            child: _buildTextComposer(),                       //modified
+          ),                                                        //new
+        ],                                                          //new
+      ),                                                            //new
+    );
+  }
+}
 
-      body: _buildTextComposer(),
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0])),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
